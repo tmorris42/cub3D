742159683f571951a17e45395bae9c8e5613418e
tmorris <tmorris@student.42.fr>
2020-12-13 23:48:58 +0100

    Create player struct and text based graphics

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h> //remove this
 #include <stdlib.h> //for exit() EXIT_SUCCESS, EXIT_FAILURE
 #include "header.h"
-//#include "libft/libft.h"
+#include "libft/libft.h"
 
 #define WIDTH	300
 #define HEIGHT	200
@@ -16,10 +16,19 @@
 #define K_S			115
 #define K_D			100
 
+typedef struct		s_player
+{
+	int		x;
+	int		y;
+	double facing; //radians
+}					t_player;
+
 typedef struct		s_screen
 {
 	void	*mlx;
 	void	*win;
+	char	map_data[5][5];
+	t_player	*player;
 }					t_screen;
 
 int		ft_get_color(int r, int g, int b, int alpha)
@@ -29,6 +38,29 @@ int		ft_get_color(int r, int g, int b, int alpha)
 
 int		ft_draw(t_screen *screen)
 {
+	int		x;
+	int		y;
+	char	print[6];
+
+	mlx_clear_window(screen->mlx, screen->win);
+	print[5] = '\0';
+	y = 0;
+	while (y < 5)
+	{
+		x = 0;
+		while (x < 5)
+		{
+//			ft_printf("map_data[%d,%d] = %c\n", x, y, (screen->map_data)[y][x]);
+			if (screen->player->x == x && screen->player->y == y)
+				print[x] = '*';
+			else
+				print[x] = screen->map_data[y][x];
+			++x;
+		}
+//		ft_printf("%s\n", print);
+		mlx_string_put(screen->mlx, screen->win, 10, 10 + 10 * y, 255, print);
+		++y;
+	}
 	mlx_pixel_put(screen->mlx, screen->win, 10, 10, ft_get_color(255,0,0,0));
 //	mlx_string_put(screen->mlx, screen->win, 10, 10, 255, "This is Blue");
 //	mlx_string_put(screen->mlx, screen->win, 10, 20, get_color(0, 255, 0), "This is Green");
@@ -51,15 +83,60 @@ int		ft_parse_keys(int	key, t_screen *screen)
 {
 	if (key == K_ESCAPE)
 		ft_close_screen(screen->win, screen);
+	else if (key == K_UP)
+		screen->player->y -= 1;
+	else if (key == K_DOWN)
+		screen->player->y += 1;
+	else if (key == K_LEFT)	//should be turn left
+		screen->player->x -= 1;
+	else if (key == K_RIGHT) //should be turn right
+		screen->player->x += 1;
 	else
 		printf(" : %d\n", key);
 	return (1);
 }
 
+t_screen	set_map(t_screen screen)
+{
+    screen.map_data[0][0] = '1';
+    screen.map_data[0][1] = '1';
+	screen.map_data[0][2] = '1';
+	screen.map_data[0][3] = '1';
+	screen.map_data[0][4] = '1';
+	screen.map_data[1][0] = '1';
+	screen.map_data[1][1] = '0';
+	screen.map_data[1][2] = '0';
+	screen.map_data[1][3] = '0';
+	screen.map_data[1][4] = '1';
+	screen.map_data[2][0] = '1';
+	screen.map_data[2][1] = '0';
+	screen.map_data[2][2] = '0';
+	screen.map_data[2][3] = '0';
+	screen.map_data[2][4] = '1';
+	screen.map_data[3][0] = '1';
+	screen.map_data[3][1] = '0';
+	screen.map_data[3][2] = '0';
+	screen.map_data[3][3] = 'N';
+	screen.map_data[3][4] = '1';
+	screen.map_data[4][0] = '1';
+	screen.map_data[4][1] = '1';
+	screen.map_data[4][2] = '1';
+	screen.map_data[4][3] = '1';
+	screen.map_data[4][4] = '1';
+	return (screen);
+}
+
 int		main(void)
 {
 	t_screen	screen;
+	t_player	player;
 
+	player.x = 3;
+	player.y = 3;
+	player.facing = 0.0;
+    
+	screen = set_map(screen);	
+	screen.player = &player;
 	screen.mlx = mlx_init();
 	if (!screen.mlx)
 		return (0);
@@ -69,6 +146,7 @@ int		main(void)
 
 	mlx_expose_hook(screen.win, &ft_draw, &screen);
 	mlx_hook(screen.win, 2, 1L<<0, &ft_parse_keys, &screen);
+	mlx_loop_hook(screen.mlx, &ft_draw, &screen);
 
 	// Wait so user can see window
 	mlx_loop(screen.mlx);
