c492bb6ddb616be423a56426bd8b039c05b1990c
tmorris <tmorris@student.42.fr>
2020-12-27 19:05:46 +0100

    Norm screenshot.c

--- Makefile
+++ Makefile
@@ -1,16 +1,27 @@
 NAME = cub3d
 
+LIBFT_DIR = ./libft/
+LIBFT = libft.a
+MLX_DIR = ./minilibx-linux/
+MLX = libmlx.a
+
 SRCS = main.c parser.c screenshot.c
 
 OBJS = ${SRCS:.c=.o}
 
-$(NAME): $(OBJS)
-	gcc $(OBJS) -L./libft/ -lft -L./minilibx-linux/ -lmlx -lXext -lX11 -lbsd -lm -o $(NAME)
+all: $(NAME)
+
+$(LIBFT_DIR)$(LIBFT):
+	$(MAKE) bonus -C $(LIBFT_DIR)
+
+$(MLX_DIR)$(MLX):
+	$(MAKE) -C $(MLX_DIR)
+
+$(NAME): $(OBJS) $(LIBFT_DIR)$(LIBFT) $(MLX_DIR)$(MLX)
+	gcc $(OBJS) -L$(LIBFT_DIR) -lft -L$(MLX_DIR) -lmlx -lXext -lX11 -lbsd -lm -o $(NAME)
 
 $(OBJS): %.o : %.c
 	gcc -c $< -o $@
-
-all: $(NAME)
 
 clean:
 	rm -f $(OBJS)
@@ -25,4 +36,5 @@ debug:
 	gcc -g3 $(OBJS) -L./libft/ -lft -L./minilibx-linux/ -lmlx -lXext -lX11 -lbsd -lm -o debug.o -fsanitize=address
 	rm -f $(OBJS)
 
-.PHONY: all clean fclean re debug
+.PHONY: all clean fclean re debug $(LIBFT_DIR)$(LIBFT) $(MLX_DIR)$(MLX)
+
--- screenshot.c
+++ screenshot.c
@@ -6,21 +6,11 @@
 /*   By: tmorris <tmorris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 20:00:44 by tmorris           #+#    #+#             */
-/*   Updated: 2020/12/27 17:46:08 by tmorris          ###   ########.fr       */
+/*   Updated: 2020/12/27 19:01:34 by tmorris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
-
-static int	ft_putint_fd(int x, int fd)
-{
-//	unsigned char	byte;
-
-//	byte = 0;
-//	byte = (unsigned char)x;
-	write(fd, &x, sizeof(x));
-	return (1);
-}
 
 static int	ft_puthex_pair_fd(int x, int fd)
 {
@@ -62,72 +52,46 @@ static int	ft_print_info(int fd, t_screen *screen)
 
 static int	ft_print_pixels(int fd, t_screen *screen)
 {
-	int		i;
-	int		max;
-	char	c;
-	int		x;
-	int		y;
+	int				max;
+	char			c;
+	int				x;
+	int				y;
 	unsigned int	pixel;
 
 	max = screen->width * screen->height;
-	i = 0;
 	x = 0;
 	y = screen->height - 1;
-	while (i < max)
+	while (y >= 0)
 	{
 		pixel = ft_get_pixel_from_image(&screen->buf, x, y);
-//		ft_printf("PIXEL =========== %d\n", pixel);
-//		if (i % 2 == 0)
-//			c = 0;
-//		else
-//			c = 255;
-//			ft_putint_fd(0, fd);
-//		else
-//			ft_putint_fd(255, fd);
 		ft_putchar_fd((char)(pixel & 0xFF), fd);
 		ft_putchar_fd((char)((pixel >> 8) & 0xFF), fd);
 		ft_putchar_fd((char)((pixel >> 16) & 0xFF), fd);
-//		ft_putchar_fd(255 * (pixel > 0), fd);
-//		ft_putchar_fd(pixel > 0, fd);
-//		ft_putchar_fd(pixel > 0, fd);
-		++x;
+		x++;
 		if (x >= screen->width)
 		{
-			x = (4 - ((max * 3) % 4)) * ((max * 3) % 4 != 0);
-			while (x > 0)
-			{
+			x = (4 - ((max * 3) % 4)) * ((max * 3) % 4 != 0) + 1;
+			while (--x > 0)
 				ft_putchar_fd(0, fd);
-				--x;
-			}
 			--y;
 		}
-		++i;
 	}
 	return (1);
 }
 
-void		ft_print_excess(int fd)
-{
-	ft_putchar_fd(0, fd);
-	ft_putchar_fd(0, fd);
-}
-
-int		ft_save(t_screen *screen, char *filename)
+int			ft_save(t_screen *screen, char *filename)
 {
 	int		fd;
-	//open file
+
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	if (fd == -1)
-	{
-//		error and quit
-		ft_printf("open error\n");
 		return (-1);
-	}
 	ft_printf("SAVING to '%s'\n", filename);
 	ft_print_header(fd);
 	ft_print_info(fd, screen);
 	ft_print_pixels(fd, screen);
-	ft_print_excess(fd);
+	ft_putchar_fd(0, fd);
+	ft_putchar_fd(0, fd);
 	close(fd);
 	return (1);
 }
